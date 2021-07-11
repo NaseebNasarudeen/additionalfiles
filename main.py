61520bc5594cc981355dc0bc235d6a1c07559a07
Amj367 <46791019+Amj367@users.noreply.github.com>
2021-07-12 02:46:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter.tix import *
 from tkinter import ttk
+import os
 
 # from PIL import ImageTk, Image
 from viewTable import dataTB
@@ -11,16 +13,62 @@ from viewTable import dataTB
 class VaccinationApp:
     def __init__(self, master):
         self.master = master
+        self.homeText = """Welcome to Quick Jabs\n\n
+The main purpose of this platform is to provide accurate and relevant information regarding COVID-19 vaccines. 
+It is a systematic vaccination registration portal for booking your vaccines appointments. 
+To get a free COVID-19 vaccine you can book an appointment at associate hospitals.
+If you are concerned about potential side-effects of the vaccine or existing health conditions, 
+it is advised to consult your family doctor before booking your appointments.
+Anyone who holds a valid health insurance and is over 18 years of age can book an appointment in their own state.
+
+Why should you get vaccinated?
+As we all know COVID-19 pandemic has ravaged national economies as well as health facilities the world over in the span of a year. 
+It is a highly contagious disease that can cause lingering health issues or even death.
+If you get vaccinated against COVID-19, you can protect yourself and others from complications caused by the Coronavirus.
+It is not mandatory to take the vaccine, but Robert Koch Institute has strongly advised the general public to do so. 
+
+You should delay your COVID-19 vaccine when:
+1. Currently infected with COVID-19 and/or until it has been at least 6 months since you have recovered from COVID-19.
+2. Have symptoms of COVID-19. 
+3. Have a fever (above 38°C), wait until you better.
+Note: Change your appointment If you cannot attend your appointment, you can ask for a new one. 
+To do this go to modify/cancel appointments.
+
+
+Personal information and Data Privacy
+Your personal information will be processed in line with the General Data Protection Laws of Germany.
+
+Contact: +49 12345678987
+Address: Karl Marx Straße 12,
+    13347 Berlin
+"""
         self.master.protocol("WM_DELETE_WINDOW", self._closeApp)
         master.title("Vaccination Buddy")
         # master.config(bg="gray17")
         master.geometry("850x750")
-
+        filePath = os.path.dirname(os.path.realpath(__file__))
         # Images
-        homeImage = PhotoImage(
-            file=r"G:\naseeb\PDFextract_text-main\PDFextract_text-main\starterFiles\finalFolder\home-icon-silhouette.gif"
+        smallHomeImage = PhotoImage(
+            file=os.path.join(filePath, "home-icon-silhouette.png")
+        ).subsample(10, 10)
+        syringeImage = PhotoImage(file=os.path.join(filePath, "syringe.png")).subsample(
+            10, 10
         )
-        smallHomeImage = homeImage.subsample(5, 5)
+        searchImage = PhotoImage(file=os.path.join(filePath, "search.png")).subsample(
+            10, 10
+        )
+        editImage = PhotoImage(file=os.path.join(filePath, "edit.png")).subsample(
+            10, 10
+        )
+        viewImage = PhotoImage(file=os.path.join(filePath, "view.png")).subsample(
+            10, 10
+        )
+        infoImage = PhotoImage(file=os.path.join(filePath, "info.png")).subsample(
+            10, 10
+        )
+        exitImage = PhotoImage(file=os.path.join(filePath, "logout.png")).subsample(
+            10, 10
+        )
 
         sidebar = Frame(master, width=100, bg="#00ADB5", height=750, borderwidth=2)
         sidebarTop = Frame(sidebar, width=100, bg="#00ADB5", height=325)
@@ -36,24 +84,37 @@ class VaccinationApp:
         # Buttons
         homeButton = Button(
             sidebarTop,
-            text="Home",
             image=smallHomeImage,
             command=lambda: homeFrame.tkraise(),
         )
+        homeButton.image = smallHomeImage
 
-        newButton = Button(sidebarTop, text="New", command=lambda: newFrame.tkraise())
+        newButton = Button(sidebarTop,image=syringeImage, command=lambda: newFrame.tkraise())
+        newButton.image = syringeImage
         searchButton = Button(
-            sidebarTop, text="search", command=lambda: searchFrame.tkraise()
+            sidebarTop, image=searchImage, command=lambda: searchFrame.tkraise()
         )
+        searchButton.image = searchImage
         modifyButton = Button(
-            sidebarTop, text="Modify", command=lambda: modifyFrame.tkraise()
+            sidebarTop,image=editImage, command=lambda: modifyFrame.tkraise()
         )
+        modifyButton.image = editImage
         viewButton = Button(
-            sidebarTop, text="View", command=lambda: viewFrame.tkraise()
+            sidebarTop, image=viewImage, command=lambda: viewFrame.tkraise()
         )
-        infoButton = Button(sidebarBottom, text="Info", command=self._infoMessage)
-        exitButton = Button(sidebarBottom, text="Exit", command=self._closeApp)
-        homeButton.pack(expand=True, fill="both", side="top")
+        viewButton.image = viewImage 
+        infoButton = Button(sidebarBottom, image=infoImage, command=self._infoMessage)
+        infoButton.image = infoImage
+        exitButton = Button(sidebarBottom, image=exitImage, command=self._closeApp)
+        exitButton.image = exitImage
+        self.changeOnHover(sidebarTop, homeButton, "Home Screen")
+        self.changeOnHover(sidebarTop, newButton, "New Registration")
+        self.changeOnHover(sidebarTop, searchButton, "Search Centers")
+        self.changeOnHover(sidebarTop, modifyButton, "Modify/Delete Registration")
+        self.changeOnHover(sidebarTop, viewButton, "View Appointments")
+        self.changeOnHover(sidebarBottom, infoButton, "Info")
+        self.changeOnHover(sidebarBottom, exitButton, "Exit")
+        homeButton.pack(expand=False, fill="both", side="top")
         newButton.pack(expand=False, fill="both", side="top")
         searchButton.pack(expand=False, fill="both", side="top")
         modifyButton.pack(expand=False, fill="both", side="top")
@@ -63,7 +124,7 @@ class VaccinationApp:
 
         # Home Frame
         homeFrame = Frame(mainarea, width=750, height=750)
-        Label(homeFrame, text="This is Home Frame").pack(
+        Label(homeFrame, text=self.homeText).pack(
             fill="both", expand=True, anchor="center"
         )
         homeFrame.grid(row=0, column=0, sticky="nsew")
@@ -109,6 +170,10 @@ class VaccinationApp:
         if op == "yes":
             self.master.destroy()
 
+    def changeOnHover(self, parent, myButton, texta):
+        tip = Balloon(parent)
+        tip.bind_widget(myButton, balloonmsg=texta)
+
     def _infoMessage(self):
         messagebox.showinfo(
             title="Information",
@@ -128,32 +193,5 @@ def main():
 if __name__ == "__main__":
     main()
 
-# root = tk.Tk()
 
 
-# root.title("Vaccination Buddy")
-# root.config(bg="gray17")
-# root.geometry("800x600")
-# sideFrame = tk.Frame(root, bg="#00ADB5")
-# mainFrame = tk.Frame(root)
-
-# homeButton = tk.Button(sideFrame, text="HM", command=lambda: print("Hello World"))
-# newButton = tk.Button(sideFrame, text="NEW", command=lambda: print("NEW DATA"))
-# searchButton = tk.Button(sideFrame, text="HM", command=lambda: print("search "))
-# modifyButton = tk.Button(sideFrame, text="HM", command=lambda: print("modify"))
-# viewButton = tk.Button(sideFrame, text="HM", command=lambda: print("view"))
-# infoButton = tk.Button(sideFrame, text="HM", command=lambda: print("info"))
-# exitButton = tk.Button(sideFrame, text="HM", command=lambda: print("exit"))
-# homeButton.grid(row=0, column=0)
-# newButton.grid(row=1, column=0)
-# searchButton.grid(row=2, column=0)
-# modifyButton.grid(row=3, column=0)
-# viewButton.grid(row=4, column=0)
-# infoButton.grid(row=7, column=0)
-# exitButton.grid(row=8, column=0)
-# # homeButton.place(x=0, y=0)
-# sideFrame.pack(side="left")
-# mainFrame.pack(side="right")
-
-
-# root.mainloop()
